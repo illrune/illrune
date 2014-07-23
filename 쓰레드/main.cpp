361d43c917d16987233d6e9e3842ee13cffcc89b
illrune <yuyu9590@naver.com>
2014-07-24 01:44:05 +0900

    Revert "멀티 쓰레드"
    This reverts commit 34f5b2be54d868bb2ee7ed4a6f7c9227951b97de.

--- 쓰레드/main.cpp
+++ 쓰레드/main.cpp
@@ -1,112 +1,44 @@
 #include <iostream>
 #include <conio.h>
-#include <string>
+#include <cstring>
 
 #include <windows.h>
 #include <process.h>
 
 using namespace std;
 
-struct UserInfo
+struct stru
 {
-	std::string name;
-	int age;
-	int time;
-
-	friend std::ostream& operator << (std::ostream& os, const UserInfo& obj)
-	{
-		os << obj.name << ' ' << obj.age << endl;
-
-		return os;
-	}
-	friend std::istream& operator >> (std::istream& is, UserInfo& obj)
-	{
-		is >> obj.name;
-		is >> obj.age;
-		is >> obj.time;
-
-		return is;
-	}
+	char a[20];
+	int b;
 };
 
 unsigned __stdcall foo(void* arg)
 {
 	DWORD tick = ::GetTickCount();
 
-	cout << "foo called" << endl;
-
-	UserInfo* Info = (UserInfo*)arg;
-
-	while(::GetTickCount() - tick < unsigned(Info->time))
+	while(::GetTickCount() - tick < 1000)
 	{
 		cout << "hello ";
-		cout << *Info;
+		//cout << *((char*)arg) << ' ';
+		cout << *((int*)arg) << endl;
 	}
-
-	cout << "end thread : " << Info->name << endl;
 
 	return 0;
 }
 
-void bar()
-{
-	int tcount = 0;
-
-	UserInfo* info0 = new UserInfo;
-	cin >> *info0;
-
-	UserInfo* info1 = new UserInfo;
-	cin >> *info1;
-
-	UserInfo* info2 = new UserInfo;
-	cin >> *info2;
-
-	unsigned id[3];
-	HANDLE hThread[3];
-
-	hThread[0] = (HANDLE)::_beginthreadex(NULL,0,&foo,info0,0,&id[0]);
-	hThread[1] = (HANDLE)::_beginthreadex(NULL,0,&foo,info1,0,&id[1]);
-	hThread[2] = (HANDLE)::_beginthreadex(NULL,0,&foo,info2,0,&id[2]);
-
-	do{
-		if (::WaitForMultipleObjects(3-tcount, hThread, FALSE, INFINITE) == WAIT_OBJECT_0)
-		{
-			if (info0)
-			{
-				cout << "delete info 0" << endl;
-				delete info0;
-				info0 = NULL;
-				tcount++;
-			}
-		}
-
-		if (::WaitForMultipleObjects(3-tcount, hThread, FALSE, INFINITE) == WAIT_OBJECT_0 + 1)
-		{
-			if (info1)
-			{
-				cout << "delete info 1" << endl;
-				delete info1;
-				info1 = NULL;
-				tcount++;
-			}
-		}
-
-		if (::WaitForMultipleObjects(3-tcount, hThread, FALSE, INFINITE) == WAIT_OBJECT_0 + 2)
-		{
-			if (info2)
-			{
-				cout << "delete info 2" << endl;
-				delete info2;
-				info2 = NULL;
-				tcount++;
-			}
-		}
-	}while(tcount < 3);
-}
-
 int main(void)
-{	
-	bar();
+{
+	stru hi;
+
+	cout << "이름입력 : ";
+	cin >> hi.a;
+
+	cout << "나이입력 : ";
+	cin >> hi.b;
+
+	unsigned id;
+	HANDLE hThread = (HANDLE)::_beginthreadex(NULL,0,&foo,&hi,0,&id);
 
 	_getch();
 	return 0;
